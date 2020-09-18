39b729447774ede95fc27a0315f3eb9633839782
Ravi Allishe <raviallishe@gmail.com>
2020-09-19 00:23:54 +0530

    misc

--- app/config/ApplicationConfig.scala
+++ app/config/ApplicationConfig.scala
@@ -7,10 +7,10 @@ object ApplicationConfig {
   // to-do - put below keys in config
   val twitterResponseTweetCount = 5
 
-  val twitterAuthToken = "3548573954-gC77ml5ChX0hUqQeH1ujwVn5EAzOdiKC0RcQlKx"
-  val twitterAuthSecret = "QQBk0soJdVjbC38NX1DZWEKrEMyENumZIQI6ojITiACZE"
-  val twitterConsumerKey = "O3d7YeEPZz56CL7U6Ss5pyJzh"
-  val twitterConsumerSecret = "TdcLkG70M7vN1BkrHQNbP9QHeKBwy4I2Yso4SSKwCI4bXw1glS"
+  val twitterAuthToken = "******-******"
+  val twitterAuthSecret = "******"
+  val twitterConsumerKey = "******"
+  val twitterConsumerSecret = "******"
 
   val oAuth1 = new OAuth1(
     twitterConsumerKey,
